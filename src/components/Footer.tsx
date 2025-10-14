f8617fc8e2ae92450f9e686aa65d25680449769b
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-10-15 00:27:13 +0200

    mail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ function Footer() {
     <footer className="mt-16 border-t border-neutral-200/60 dark:border-neutral-800/60">
       <div className="w-full px-4 sm:px-6 lg:px-10 py-6 text-sm text-neutral-600 dark:text-neutral-400 flex items-center justify-between">
         <span>© {year} • Alle Rechte vorbehalten.</span>
-        {/* kontakt information mustafa.ozdmir1408@gmail.com */}
-        <span>Kontakt: <a href="mailto:mustafa.ozdmir1408@gmail.com">mustafa.ozdmir1408@gmail.com</a></span>
+        {/* kontakt information mustafa.ozdemir1408@gmail.com */}
+        <span>Kontakt: <a href="mailto:mustafa.ozdemir1408@gmail.com">mustafa.ozdemir1408@gmail.com</a></span>
         <a
           href="https://github.com/1DeliDolu/pehlione.io"
           target="_blank"
